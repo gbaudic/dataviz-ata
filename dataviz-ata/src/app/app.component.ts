1bacfdff5c090d97b5ceed945b0a86bfc612feed
gbaudic <gbaudic@users.noreply.github.com>
2018-05-03 19:57:54 +0200

    Adding example codes from bootstrap and leaflet

--- dataviz-ata/src/app/app.component.ts
+++ dataviz-ata/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  mymap: any;
+  
+  constructor() {
+    // This belongs to an ngOnInit instead
+    this.mymap = L.map('mapid').setView([46.449, 2.210], 6);
+
+	L.tileLayer('//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+      attribution: 'donn&eacute;es &copy; <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+      minZoom: 1,
+      maxZoom: 20
+    }).addTo(map);
+  }
 }
