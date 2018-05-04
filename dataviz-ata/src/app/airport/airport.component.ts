768fc665197b85eff9d83f483dd199619d0d0067
Gwilherm Baudic <gbaudic@users.noreply.github.com>
2018-05-04 11:28:15 +0200

    Adding airport description class

--- dataviz-ata/src/app/airport/airport.component.ts
+++ dataviz-ata/src/app/airport/airport.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./airport.component.css']
 })
 export class AirportComponent implements OnInit {
+  currentAirport: AirportDescription;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+export class AirportDescription {
+  oaci: string;
+  code: string;  // 3-letter - replace with correct name
+  name: string;
+  country: string;
+  lat: number;
+  long: number;
 }
