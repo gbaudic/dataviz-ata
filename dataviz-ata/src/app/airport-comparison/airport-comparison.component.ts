e8a064d6b4994098d58812b8201d95f512db9f58
Gwilherm Baudic <gbaudic@users.noreply.github.com>
2018-06-03 23:38:09 +0200

    More top10 in comparison

--- dataviz-ata/src/app/airport-comparison/airport-comparison.component.ts
+++ dataviz-ata/src/app/airport-comparison/airport-comparison.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AirportListService } from 'app/airport-list.service';
+import { TrafficService } from 'app/traffic.service';
 import { AirportDescription } from 'app/airport/airport.component';
 import { AirportDetailsComponent } from 'app/airport-details/airport-details.component';
 
@@ -15,8 +16,11 @@ export class AirportComparisonComponent implements OnInit {
   airport1: string;
   airport2: string;
   airports: AirportDescription[] = [];
+  airport1_top10: Array<[string, number]>;
+  airport2_top10: Array<[string, number]>;
 
-  constructor(private listServ: AirportListService) { }
+  constructor(private listServ: AirportListService,
+    private top10Serv: TrafficService) { }
 
   ngOnInit() {
     // load airport list from service to populate selects
