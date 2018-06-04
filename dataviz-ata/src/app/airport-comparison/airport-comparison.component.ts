3363600a94406b91c4de331aec647555edf8388f
Gwilherm Baudic <gbaudic@users.noreply.github.com>
2018-06-04 21:07:57 +0200

    Started including description in comparison #2

--- dataviz-ata/src/app/airport-comparison/airport-comparison.component.ts
+++ dataviz-ata/src/app/airport-comparison/airport-comparison.component.ts
@@ -3,6 +3,8 @@ import { AirportListService } from 'app/airport-list.service';
 import { TrafficService } from 'app/traffic.service';
 import { AirportDescription } from 'app/airport/airport.component';
 import { AirportDetailsComponent } from 'app/airport-details/airport-details.component';
+import fontawesome from '@fortawesome/fontawesome';
+import * as faCalendarAlt from '@fortawesome/fontawesome-free-solid/faCalendarAlt';
 
 @Component({
   selector: 'app-airport-comparison',
@@ -18,13 +20,20 @@ export class AirportComparisonComponent implements OnInit {
   airports: AirportDescription[] = [];
   airport1_top10: Array<[string, number]>;
   airport2_top10: Array<[string, number]>;
+  selectedAirportsDescription: AirportDescription[] = [];
 
   constructor(private listServ: AirportListService,
-    private top10Serv: TrafficService) { }
+    private top10Serv: TrafficService) {
+    fontawesome.library.add(faCalendarAlt);
+  }
 
   ngOnInit() {
     // load airport list from service to populate selects
-    this.listServ.getAirportList().subscribe(res => this.airports = res);
+    this.listServ.getAirportList().subscribe(res => {
+      this.airports = res;
+      this.selectedAirportsDescription.push(this.airports[0]);
+      this.selectedAirportsDescription.push(this.airports[0]);
+    });
   }
 
   findDescription(oaci: string): AirportDescription {
@@ -33,6 +42,18 @@ export class AirportComparisonComponent implements OnInit {
         return ap;
       }
     }
+  }
+
+  loadExtra(event: any, index: number) {
+    this.selectedAirportsDescription[index - 1] = this.findDescription(event);
+
+    this.top10Serv.getTop10(event).subscribe(res => {
+      if (index == 1) {
+        this.airport1_top10 = res;
+      } else {
+        this.airport2_top10 = res;
+      }
+    });
   }
 
 }
